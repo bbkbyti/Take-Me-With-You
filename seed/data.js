3f311c87a7c8504422485c729e5eb69af27a7029
Babak Bayati <bbkbyti@Babaks-MacBook-Pro.local>
2021-08-03 00:01:11 -0500

    added new data to database to test functionality

--- seed/data.js
+++ seed/data.js
@@ -38,6 +38,24 @@ const insertData = async () => {
         'https://dallaszoo.imgix.net/wp-content/uploads/2019/10/30120014/MG_1398-Dallas-Zoo-Entry-CB.jpg?fit=crop&crop=faces&auto=format&q=60&w=395&h=198',
       userId: user2,
     },
+    {
+      country: 'Turkey',
+      city: 'Istanbul',
+      description: 'beautiful city',
+      location: 'Taksim Square',
+      imgURL:
+      'https://images.unsplash.com/photo-1622587853578-dd1bf9608d26?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+      userId: user2,
+    },
+
+    {
+      country: 'Italy',
+      city: 'Rome',
+      description: 'The Colosseum (/ˌkɒləˈsiːəm/ KOL-Ə-SEE-Əm; Italian: Colosseo [Kolosˈsɛːo]) Is An Oval Amphitheatre In The Centre Of The City Of Rome, Italy, Just East Of The Roman Forum.',
+      imgURL: 'https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1286&q=80',
+      userId: user1,
+      location: "Colosseum"
+    },
   ];
 
   await Trip.insertMany(trips);
