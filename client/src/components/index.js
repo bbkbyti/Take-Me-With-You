dd867b795c5b16ab7b2951f9d837212f4986430e
arybyt <bayatiarya@gmail.com>
2021-08-02 19:59:57 -0500

    set up client folder and subfolders and added tailwindCSS

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import Button from './Button/Button';
+import Footer from './Footer/Footer';
+import Layout from './Layout/Layout';
+import Nav from './Nav/Nav';
+import Trip from './Product/Product';
+import TripCard from './ProductCard/ProductCard';
+import TripForm from './ProductForm/ProductForm';
+import SignIn from './SignIn/SignIn';
+import SignUp from './SignUp/SignUp';
+import SignOut from './SignOut/SignOut';
+
+export { Button, Footer, Layout, Nav, Trip, TripCard, TripForm, SignIn, SignUp, SignOut };
